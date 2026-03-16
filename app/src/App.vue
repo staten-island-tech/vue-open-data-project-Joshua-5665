188e47ca2a0d7f11535368f1b3cc5e756807dd58
Joshua-5665 <joshuac726@nycstudents.net>
2026-03-16 14:02:34 -0400

    loo

--- app/src/App.vue
+++ app/src/App.vue
@@ -5,7 +5,13 @@
 </template>
 
 <script setup>
-  
+  import {onMounted, ref, watch} from 'vue'
+  import {useRoute} from 'vue-router'
+
+  async function getData {
+    const response = await fetch(`https://data.cityofnewyork.us/resource/xi7c-iiu2.json`)
+    const data = await response.json()
+  }
 </script>
 
 <style scoped>
