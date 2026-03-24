0844b0c6c8e21c80498a45c7713fa1d82fdff460
Joshua-5665 <joshuac726@nycstudents.net>
2026-03-24 14:02:03 -0400

    jjj

--- app/src/components/locationCard.vue
+++ app/src/components/locationCard.vue
@@ -1,7 +1,11 @@
 <template>
+    <div class="years">
+   
+    </div>
+
     <router-link :to="dataPath" class="card">
-        <h2>{{ loc.borough }}</h2>
-        <h2>{{ loc.cd_number }}</h2>
+        <h2>Year: {{ loc.year }}</h2>
+        <h2>NYC Population: {{ loc.new_york_city_population }}</h2>
     </router-link>
 </template>
 
@@ -20,6 +24,14 @@
 
 <style scoped>
 .card{
+    height: 200px;
+    width: 28%;
     display: flex;
+    flex-direction: column;
+    text-align: center;
+    background-color: rgb(228, 255, 223);
+}
+.years{
+    font-size: 20px;
 }
 </style>
--- app/src/views/locationData.vue
+++ app/src/views/locationData.vue
@@ -1,5 +1,24 @@
 <template>
     <div>
+       <label>Select Year:
+       <select v-model="selected">
+        <option value="2024">2024</option>
+        <option value="2023">2023</option>
+        <option value="2022">2022</option>
+        <option value="2021">2021</option>
+        <option value="2020">2020</option>
+        <option value="2019">2019</option>
+        <option value="2018">2018</option>
+        <option value="2017">2017</option>
+        <option value="2016">2016</option>
+        <option value="2015">2015</option>
+        <option value="2014">2014</option>
+        <option value="2013">2013</option>
+        <option value="2012">2012</option>
+        <option value="2011">2011</option>
+        <option value="2010">2010</option>
+       </select>
+    </label>
       <locationCard v-for="kor in loc" :key="kor.cd_number" :loc="kor"/>
     </div>
 </template>
@@ -8,10 +27,11 @@
   import locationCard from '@/components/locationCard.vue'
   import {onMounted, ref, watch} from 'vue'
   import {useRoute} from 'vue-router'
+  const selected = ref("2024")
   const route = useRoute()
   const loc = ref({})
   async function getData(id) { 
-    const response = await fetch(`https://data.cityofnewyork.us/resource/xi7c-iiu2.json`)
+    const response = await fetch(`https://data.cityofnewyork.us/resource/ia2d-e54m.json`)
     const data = await response.json()
     loc.value = data
   }
@@ -24,6 +44,7 @@
       getData(id)
     },
   )
+
 </script>
 
 <style scoped>
